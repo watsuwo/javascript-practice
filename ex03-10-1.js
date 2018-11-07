83b4633a75753da7b10095ee7186b401f23af307
watsuwo <watsuwo119@gmail.com>
2018-11-07 01:59:35 +0000

    ex01-10-1

---
+++ ex03-10-1.js
@@ -0,0 +1,13 @@
+let currentTemp;
+console.log(currentTemp);
+
+currentTemp = null;
+console.log(currentTemp);
+
+currentTemp = 25;
+console.log(currentTemp);
+
+currentTemp = undefined;
+console.log(currentTemp);
+
+
