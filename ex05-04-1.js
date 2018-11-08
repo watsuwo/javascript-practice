fabf841b1c5a8618d344765721b8a2b174918af5
watsuwo <watsuwo119@gmail.com>
2018-11-08 02:56:50 +0000

    2個した

---
+++ ex05-04-1.js
@@ -0,0 +1,15 @@
+const n = 5, s = "5";
+
+console.log(n === s);
+console.log(n !== s);
+console.log(n == s);
+console.log(n != s);
+console.log(n === Number(s));
+console.log(n !== Number(s));
+
+const a = { name: "あるオブジェクト"};
+const b = { name: "あるオブジェクト"};
+console.log(a === b);
+console.log(a !== b);
+console.log(a == b);
+console.log(a != b);
---
+++ ex05-04-3.js
@@ -0,0 +1,5 @@
+console.log(NaN === NaN);
+console.log(NaN !== NaN);
+console.log(isNaN(NaN));
+console.log(isNaN(3));
+console.log(isNaN("aaa"));
