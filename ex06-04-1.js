68ddd4f58b44a1bdd21ad92566b44a1c2a253869
watsuwo <watsuwo119@gmail.com>
2018-11-10 01:47:33 +0000

    6.4.1

---
+++ ex06-04-1.js
@@ -0,0 +1,7 @@
+const o = {
+    name: "aaa",
+    bark: function(){return "WoW";}
+};
+
+console.log(o);
+console.log(o.bark());
---
+++ exi06-04-2.js
@@ -0,0 +1,7 @@
+const o = {
+    name: "pochi",
+    bark(){return "WoW";}
+};
+
+console.log(o);
+console.log(o.bark());
