adc6255cb3210d340c37e5620314734c7325ea59
watsuwo <watsuwo119@gmail.com>
2018-11-11 10:05:00 +0000

    7.7

---
+++ 07/ex07-06-3.js
@@ -0,0 +1,15 @@
+const message =  (function(){
+    const secret = "私は秘密です";
+    return `秘密の長さは､${secret.length}です`;
+})();
+
+console.log(message);
+console.log(secret);
+
+const message2 = function(){
+    const secret = "あああああ";
+    return sercret.length;
+}
+
+console.log(message2);
+
---
+++ 07/ex07-06-4.js
@@ -0,0 +1,13 @@
+const f = (function(){
+    let count=0;
+    return function(){
+      return `この関数呼び出し回数${count++}`;
+      return ciuni;
+    }
+})();
+
+console.log(f());
+console.log(f());
+console.log(f());
+console.log(f());
+console.log(f);
