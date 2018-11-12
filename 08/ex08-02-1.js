0a4777c732b4e63b0a78a05ca496169e67de9a0e
watsuwo <watsuwo119@gmail.com>
2018-11-12 13:50:59 +0000

    8.1.1

---
+++ 08/ex08-02-1.js
@@ -0,0 +1,11 @@
+let arr = ["1", "2", "3"];
+
+console.log(arr.push("4"));
+console.log(arr);
+console.log(arr.pop());
+console.log(arr);
+console.log(arr.unshift("0"));
+console.log(arr);
+console.log(arr.shift());
+console.log(arr)
+console.log(arr.unshift());
