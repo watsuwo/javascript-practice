4837d09558e4cd5d2ffa69f44897321457dc8bfb
watsuwo <watsuwo119@gmail.com>
2018-11-14 13:52:33 +0000

    map関数

--- 08/ex08-04-1.js
+++ 08/ex08-04-1.js
@@ -1,6 +1,19 @@
-const cart = [{name: "Iphone", price: 54800}];
+const cart = [{name: "Iphone", price: 54800}, {name: "Android", price: 20000}];
+
 const names = cart.map(function(x){
     return x.name;
 });
 
 console.log(names);
+
+const prices = cart.map(function(x){
+    return x.price;
+});
+
+console.log(prices);
+
+const discountPrice = cart.map(function(x){
+    return x.price * 0.8;
+});
+
+console.log(discountPrice);
