6576500d8caaea9f8867834335e9640471a4a72f
watsuwo <watsuwo119@gmail.com>
2018-11-23 10:29:07 +0000

    9.2.2

---
+++ 09/ex09-02-3.js
@@ -0,0 +1,21 @@
+class Car {
+  constructor(make, model){
+    this.make = make; //maker name
+    this.model = model; //model name
+    this._userGears = ['P', 'N', 'R', 'D'];
+    this._userGear = this._userGears[0];
+  }
+
+get userGear(){return this._userGear;}
+set userGear(value){
+  if(this._userGears.indexOf(value) < 0)
+    throw new Error(`ギア指定が正しくない: ${value}`);
+  this._userGear = value;
+}
+  shift(gear){this.userGear = gear;}
+
+} 
+
+const car1 = new Car('Tesla', 'S');
+
+console.log(car1);
