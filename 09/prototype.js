f9034671f0e5c33149e4df559dfa3b52662c29c4
watsuwo <watsuwo119@gmail.com>
2018-11-30 19:05:13 +0000

    12/1

--- 09/prototype.js
+++ 09/prototype.js
@@ -6,3 +6,16 @@ let obj = {
 console.log(Object.prototype);
 console.log(obj.__proto__ === Object.prototype);
 
+console.log(obj.name);
+
+console.log(
+    obj.toString
+    );
+
+console.log(
+    obj.__proto__.__proto__ === null
+    );
+
+console.log(
+    obj.name === "Wataru"
+    );
